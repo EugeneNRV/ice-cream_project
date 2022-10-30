e39b7e1568ff548ec0fbe2140a1b00a2aa053eb9
EugeneNRV <nrv.eugene@gmail.com>
2022-10-30 13:12:32 +0300

    save

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -3,6 +3,6 @@ console.log('privet medved');
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
 	loop: true,
-	slidesPerView: 5,
-	spaceBetween: 20,
+	slidesPerView: 3,
+	spaceBetween: 26,
 });
